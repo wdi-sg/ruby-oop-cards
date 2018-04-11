15560519ad5e32f67dc341b63bba0703fa02e7c9
jaanhio <jianhaot.89@gmail.com>
2018-04-11 17:45:43 +0800

    done

--- game.rb
+++ game.rb
@@ -4,6 +4,8 @@ class Game
   def initialize()
     @deck = Cards.new
     @player_score = 0
+    @player_hand
+    @house_hand
   end
 
   def check_player_cards
@@ -18,10 +20,26 @@ class Game
     @player_score
   end
 
+  def check_deck_size
+    @deck.check_num_of_cards
+  end
+
+  def check_remaining_card
+    @deck.check_num_of_cards
+  end
+
   def play
     @player_hand = @deck.get_card
     @house_hand = @deck.get_card
 
+    # if @player_hand > @house_hand
+    #   @player_score += 1
+    # else
+    #   @player_score -= 1
+    # end
+  end
+
+  def compare_cards
     if @player_hand > @house_hand
       @player_score += 1
     else
--- play.rb
+++ play.rb
@@ -5,27 +5,38 @@ playing = true
 
 game = Game.new
 
-while playing == true do
-  # puts "Enter your name:"
-  # player_name = gets.chomp
+puts "Enter your name:"
+player_name = gets.chomp
 
-  # puts "Welcome to card game, #{player_name}."
+puts "Welcome to card game, #{player_name}."
+
+while playing == true do
+  
 
   game.play
   puts "Your current hand is #{game.check_player_cards.to_s}"
   puts "House hand is #{game.check_house_cards.to_s}"
+  puts "Remaining cards: #{game.check_remaining_card}"
 
   puts "Do you want to play this round?"
   puts "Your current score is #{game.check_player_score}"
   answer = gets.chomp
 
+
   if answer == "no"
     puts "Skipping this round"
-    next
+    if game.check_remaining_card <= 0
+      puts "Game over. No more cards left"
+      puts "Your current score is #{game.check_player_score}."
+      playing = false
+    end
+  else
+    game.compare_cards
+    puts "Your current score is #{game.check_player_score}."
+    if game.check_player_score < -2
+      puts "Your score fell below -2. Game over"
+      playing = false
+    end
   end
 
-  if game.check_player_score < -2
-    puts "You have lost. Game over"
-    playing = false
-  end
 end
