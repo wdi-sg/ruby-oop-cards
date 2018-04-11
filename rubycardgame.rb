c0d01ae672b3edb7adfe8319d2baaea13b5e0aab
Seliyan Silvarajoo <seliyansilvarajoo@Seliyans-MacBook-Pro.local>
2018-04-11 16:57:55 +0800

    HW completed

---
+++ rubycardgame.rb
@@ -0,0 +1,72 @@
+require 'byebug'
+
+class Cards
+	attr_reader :cards
+
+	def initialize
+		@cards = []
+		52.times do |number|
+			@cards << number
+		end
+	end
+end
+
+generate_cards= Cards.new
+
+class Game
+	attr_reader :drawn_cards, :house_card, :player_card, :player_score, :deck, :play_again
+
+	def initialize()
+		@deck=Cards.new.cards
+		@player_score=0
+	end
+
+	def play
+		@drawn_cards = @deck.sample(2)
+		@house_card=drawn_cards[0]
+		@player_card=drawn_cards[1]
+		self
+	end
+
+	def checkWin
+		if @player_card > @house_card
+			@player_score +=1
+			puts @player_score
+		else 
+			@player_score -=1
+			puts "lost"
+			puts @player_score
+		end
+		puts "want to play again?"
+		@play_again=gets.chomp
+		if (@play_again=="y")
+			puts "here"
+			play
+		else 
+			puts "there"
+			until (@play_again=="y")||(@play_again=="n")
+				puts "invalid input,enter again"
+				@play_again=gets.chomp	
+			end
+		end
+		puts "HELLO"
+		self
+	end
+
+end
+
+is_game_on = true
+game = Game.new
+while is_game_on
+	game.play.checkWin
+	if game.player_score < -2
+		is_game_on=false
+	end
+	if game.play_again=="n"
+		is_game_on=false
+	end
+
+end
+
+byebug
+puts "test"
