449047a8e56c3c6f004731a902a5c176275be649
Lee Wen Yao <peterleewenyao@gmail.com>
2018-07-17 12:35:12 +0800

    homework

--- game.rb
+++ game.rb
@@ -5,7 +5,7 @@ class Cards
    end  
 
    def draw_card
-      @cards.shuffle.first
+      @cards.shuffle.pop
    end
 end
 
@@ -13,35 +13,32 @@ class Game
 
    def initialize
       @deck = Cards.new
-      @player1_score = 0
-      @player2_score = 0
+      @player_score = 0
       @hands = []
    end
 
-   def play 
-      player1_hand = @deck.draw_card
-      player2_hand = @deck.draw_card
+   def the_game
+      player_hand = @deck.draw_card
+      ai_hand = @deck.draw_card
+      @hands << player_hand
+      @hands << ai_hand
 
-      if player1_hand > player2_hand 
-         @player1_score += 1
-         @player2_score -= 1
-
-      elsif player2_hand > player1_hand
-         @player2_score += 1
-         @player1_score -= 1
-      else 
-         @player1_score = @player1_score
-         @player2_score = @player2_score
+      if player_hand > ai_hand 
+         @player_score += 1
+         puts "Player's hand: #{player_hand}, is higher than AI. Score added: #{@player_score}"
+      else
+         @player_score -= 1
+         puts "Player's hand: #{player_hand}, is lower than AI. Minus-ed score : #{@player_score}"
       end
+   end
 
-      def check_score
-         @player1_score
-         @player2_score
-
-         if player1_score < -2 || player2_score < -2
-            
-
+   def play_game
+      while @player_score > -2
+         the_game
       end
    end
 
 end
+
+cardgame = Game.new
+cardgame.play_game
