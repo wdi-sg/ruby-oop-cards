d7c9377b903fbd101ddce00c62bdd4ad90b7240d
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-10-18 22:50:54 +0800

    add: further 2

--- play.rb
+++ play.rb
@@ -6,5 +6,17 @@ puts "Please enter your name: "
 input = gets.chomp
 while game.score > -2
   game.gamePlay
-  puts "hello #{input}, you have #{game.score} points."
+  puts "hello #{input}, your current score is #{game.score}"
+  puts "Would you like to continue the game? (Y/N)"
+  ans = gets.chomp
+  if ans == "Y"
+    next
+  elsif ans == "N"
+    puts "Thanks for playing! Your final score is #{game.score}"
+    break
+  else
+    puts "Invalid reply, BYE!"
+    break
+  end
+  # puts "hello #{input}, you have #{game.score} points."
 end
