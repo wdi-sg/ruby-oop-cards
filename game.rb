b070acee5f0fb7f60cae13bd1659be064e145db3
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-18 23:48:11 +0800

    Further: Add betting feature, refactor compare_hands

--- game.rb
+++ game.rb
@@ -12,11 +12,10 @@ end
 
 # Game class that manages the card class and two players.
 class Game
-  attr_accessor :a_name, :a_score, :b_score, :turn
+  attr_accessor :player_name, :score, :turn
   def initialize(name)
-    @a_name = name
-    @a_score = 0
-    @b_score = 0
+    @name = name
+    @score = 0
     @a_hand = []
     @b_hand = []
     @history = []
@@ -32,22 +31,33 @@ class Game
 
   # Draw cards and add to player hands
   def deal(cards)
+    puts "*TURN #{@turn}*"
     @a_hand << cards.deck.shift
-    puts "#{@a_name} played #{@a_hand}"
+    puts "#{@name} played #{@a_hand}"
     @b_hand << cards.deck.shift
     puts "Banker played #{@b_hand}"
   end
 
   # Compare drawn cards and assign points. Record hands after
   def compare_hands
-    if @a_hand[0] > @b_hand[0]
-      @a_score += 1
-      @b_score -= 1
-    else
-      @b_score += 1
-      @a_score -= 1
+    if @a_hand[0] > @b_hand[0] ? (who = @name) : (who = 'Banker')
     end
-    puts "#{@a_name}: #{@a_score} points, Banker: #{@b_score} points"
+    who
+  end
+
+  def determine(who)
+    if who == 'Banker' ? @score -= 1 : @score += 1
+      puts "#{who} wins the round."
+      puts "#{@name}, you have #{@score} points."
+    end
+    record_hands
+  end
+
+  def wager(who, bet)
+    if who == 'Banker' ? @score -= bet : @score += bet
+      puts "#{who} wins the round."
+      puts "#{@name}, you have #{@score} points."
+    end
     record_hands
   end
 
@@ -63,13 +73,7 @@ class Game
   # Print out final game summary
   def report
     puts '*GAME ENDED*'
-    puts "#{@a_name} has #{@a_score} points."
-    puts "Banker has #{@b_score} points."
-    if @a_score > @b_score
-      puts "#{@a_name} has won the game!"
-    else
-      puts 'Banker has won the game!'
-    end
+    puts "#{@name} has #{@score} points."
     puts '*MATCH HISTORY*'
     @history.each { |item| puts item.join }
   end
@@ -89,14 +93,27 @@ play.shuffle_deck(cards)
 
 # Loop game while player scores not equal -2 or turn more than 26
 loop do
-  puts "*TURN #{play.turn}*"
   play.deal(cards)
-  play.compare_hands
-  break if play.a_score == -2 || play.b_score == -2
+  puts play.compare_hands
+  play.determine(play.compare_hands)
+  break if play.score == -2
 
   puts 'Do you want to continue?'
   break if gets.chomp.include? 'no'
 
+  while play.score >= 2
+    puts 'Do you want to wager your score?'
+    answer = gets.chomp
+    if answer != 'yes'
+      puts 'You chose not to wager. Game proceeds.'
+      break
+    end
+    puts "You can wager additional #{play.score - 1} point(s), Bet: "
+    wager = gets.chomp.to_i
+    puts "You bet 1 + #{wager}" unless wager.nil?
+    play.deal(cards) unless wager.nil?
+    play.wager(play.compare_hands, wager) unless wager.nil?
+  end
   play.turn += 1
   break if play.turn == 27
 end
