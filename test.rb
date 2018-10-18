9790610d4341e5efd202c9d6118a3b025bf8ed98
Rstinkho <t.turchinskiy@bk.ru>
2018-10-18 21:10:27 +0800

    added cards & game classes

--- test.rb
+++ test.rb
@@ -1,20 +1,79 @@
-playerOneScore = 0
-playerTwoScore = 0
+require 'byebug'
 
-put = 2.times.map{ Random.rand(52) }
+# playerOneScore = 0
+# playerTwoScore = 0
 
-puts put
+# put = 1.times.map{ Random.rand(52) }
 
-if (put[0] >= put[1])
-  playerOneScore += 1
-  puts "Player One + 1"
+# puts put
+
+# if (put[0] >= put[1])
+#   playerOneScore += 1
+#   puts "Player One + 1"
 
 
-  elsif (put[0] <= put[1])
-    playerTwoScore += 1
-    puts "Player Two + 1"
+#   elsif (put[0] <= put[1])
+#     playerTwoScore += 1
+#     puts "Player Two + 1"
+
+# end
+
+# puts "SCORE: Player1 = #{playerOneScore}, player2 = #{playerTwoScore}"
+
+
+#Create a card class that, when you initialize it, creates an array of "cards" that are of value 1-52.
+
+class Cards
+attr_accessor :cards
+
+  def initialize
+    @cards = Array.new(52) {|i| i+1 }
+  end
+
+  def shuffle_cards
+    @cards.shuffle
+  end
+
+  def draw_card
+    @cards.pop
+  end
 
 end
 
-puts "SCORE: Player1 = #{playerOneScore}, player2 = #{playerTwoScore}"
+
+byebug
+puts "end"
+
+#Create a game class that uses the card class to play a game.
+
+class Game
+  def initialize
+    @deck = Cards.new.cards.shuffle
+    @score = 0
+    @hands = []
+  end
+
+  def play
+    user_hand = @deck.show_card
+    house_hand = @deck.show_card
+
+    @hands << [user_hand, house_hand]
+
+    if user_hand > house_hand
+      @score += 1
+    else
+      @score -= 1
+    end
+  end
+
+  def check_game
+    @score
+  end
+
+end
+
+
+
+
+
 
