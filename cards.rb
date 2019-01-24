7e4a23104c6ae9aec036322233649c54c4c86e57
sean chan <sean@seans-MacBook-Air.local>
2019-01-24 18:29:34 +0800

    Further 1-3 add logic to betting

--- cards.rb
+++ cards.rb
@@ -33,10 +33,19 @@ class Game
     elsif player_card > computer_card
       puts 'You won'
       @score += 1
+      if @bet_amt != nil
+        @score += @bet_amt
+      end
     else
       puts 'You lost'
       @score -= 1
+      if @bet_amt != nil
+        @score -= @bet_amt
+      end
     end
+  end
+  def wager(bet)
+    @bet_amt = bet
   end
   def check_game
     @score
@@ -44,10 +53,13 @@ class Game
 end
 
 start = false
+name = ''
 
 puts 'Welcome do you want to begin?'
 input = gets.chomp
 if input == 'y'
+  puts 'Enter your name challenger'
+  name = gets.chomp
   start = true
 else
   puts 'Are you a joke?'
@@ -57,14 +69,23 @@ game = Game.new
 
 while start == true
 
-  puts "Your current score is: #{game.check_game.to_s}"
+  puts "#{name} this is your current score: #{game.check_game.to_s}"
 
-  puts "Do want to draw a card?"
+  puts "Do want to wager using your points?"
+  bet = gets.chomp
+  if bet == 'y'
+    puts 'What is the amount?'
+    bet_amt = gets.chomp.to_i
+    game.wager(bet_amt)
+    puts "Do want to draw a card?"
+  elsif bet == 'n'
+    puts "Do want to draw a card?"
+  end
 
   answer = gets.chomp
   if answer == 'n'
     puts 'Thanks for playing'
-    puts "Your accumulated score is: #{game.check_game.to_s}"
+    puts "#{name} this is your accumulated score: #{game.check_game.to_s}"
     start = false
   elsif answer == 'y'
     game.play
@@ -72,7 +93,7 @@ while start == true
   end
 
   if game.check_game < -5
-    puts 'You lost too much games so its the end'
+    puts "You lost too much score so its the end #{name}"
     start = false
   end
 end
