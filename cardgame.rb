c9dbaf1fcd8d06185eb51665676307cbfa2979a4
Chang <ainurlindale@gmail.com>
2019-04-25 00:18:04 +0800

    further 2

--- cardgame.rb
+++ cardgame.rb
@@ -2,8 +2,8 @@
 puts "What is player one name?"
 $p_one_name = gets.chomp
 
-puts "What is player two name?"
-$p_two_name = gets.chomp
+# puts "What is player two name?"
+# $p_two_name = gets.chomp
 
 class Card
 
@@ -54,7 +54,7 @@ class Game
     puts "#{$p_one_name} Card is " + @player_one_hand[0].to_s;
 
     @player_two_hand.push(@new_deck.pop);
-    puts "#{$p_two_name} Card is " +  @player_two_hand[0].to_s;
+    puts "Computer Card is " +  @player_two_hand[0].to_s;
 
     if @player_one_hand[0] > @player_two_hand[0]
 
@@ -62,17 +62,17 @@ class Game
       @player_two_point = @player_two_point - 1
       # puts @player_one_point
       puts "#{$p_one_name} point is " + @player_one_point.to_s
-      puts "#{$p_two_name} point is " + @player_two_point.to_s
+      puts "#Computer point is " + @player_two_point.to_s
      else
       # puts "Player 2 Wins"
       @player_two_point = @player_two_point + 1
       @player_one_point = @player_one_point - 1
       puts "#{$p_one_name} point is " + @player_one_point.to_s
-      puts "#{$p_two_name} point is " + @player_two_point.to_s
+      puts "#Computer point is " + @player_two_point.to_s
     end
 
     if @player_one_point == -2
-      puts "#{$p_two_name} WINS!"
+      puts "#Computer WINS!"
     elsif @player_two_point == -2
       puts "#{$p_one_name} WINS!"
     end
@@ -84,13 +84,28 @@ class Game
     # end
   end
 
+  def calculate
+    puts "#{$p_one_name} point is " + @player_one_point.to_s
+    puts "#Computer point is " + @player_two_point.to_s
+  end
+
+
 end
 
 game_round = Game.new(card_deck.cards, 0, 0);
 # print game_round.deck
 
-
-until game_round.player_one_point == -2 || game_round.player_two_point == -2
   game_round.draw
   game_round.deal
+  puts "Do you still want to continue? Y/N"
+  player_answer = gets.chomp.upcase
+
+
+until game_round.player_one_point == -2 || game_round.player_two_point == -2 || player_answer == 'N'
+  game_round.draw
+  game_round.deal
+  puts "Do you still want to continue? Y/N"
+  player_answer = gets.chomp.upcase
 end
+
+game_round.calculate
