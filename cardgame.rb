91740d0e0b960b14ae657a2d3bd21e2c2012803f
Chang <ainurlindale@gmail.com>
2019-04-25 00:01:41 +0800

    further one

--- cardgame.rb
+++ cardgame.rb
@@ -1,4 +1,9 @@
 # puts ("Hello World");
+puts "What is player one name?"
+$p_one_name = gets.chomp
+
+puts "What is player two name?"
+$p_two_name = gets.chomp
 
 class Card
 
@@ -46,30 +51,30 @@ class Game
     @player_two_hand = [];
 
     @player_one_hand.push(@new_deck.pop);
-    puts "Player 1 Card is " + @player_one_hand[0].to_s;
+    puts "#{$p_one_name} Card is " + @player_one_hand[0].to_s;
 
     @player_two_hand.push(@new_deck.pop);
-    puts "Player 2 Card is " +  @player_two_hand[0].to_s;
+    puts "#{$p_two_name} Card is " +  @player_two_hand[0].to_s;
 
     if @player_one_hand[0] > @player_two_hand[0]
 
       @player_one_point = @player_one_point + 1
       @player_two_point = @player_two_point - 1
       # puts @player_one_point
-      puts "player one point is " + @player_one_point.to_s
-      puts "player two point is " + @player_two_point.to_s
+      puts "#{$p_one_name} point is " + @player_one_point.to_s
+      puts "#{$p_two_name} point is " + @player_two_point.to_s
      else
       # puts "Player 2 Wins"
       @player_two_point = @player_two_point + 1
       @player_one_point = @player_one_point - 1
-      puts "player one point is " + @player_one_point.to_s
-      puts "player two point is " + @player_two_point.to_s
+      puts "#{$p_one_name} point is " + @player_one_point.to_s
+      puts "#{$p_two_name} point is " + @player_two_point.to_s
     end
 
     if @player_one_point == -2
-      puts "PLAYER TWO WINS!"
+      puts "#{$p_two_name} WINS!"
     elsif @player_two_point == -2
-      puts "PLAYER ONE WINS!"
+      puts "#{$p_one_name} WINS!"
     end
 
     # if @player_one_point < -2
@@ -89,12 +94,3 @@ until game_round.player_one_point == -2 || game_round.player_two_point == -2
   game_round.draw
   game_round.deal
 end
-
-
-
-# game_round.draw
-# game_round.deal
-# game_round.deal
-# game_round.deal
-# game_round.player_one_point
-# game_round.player_two_point
