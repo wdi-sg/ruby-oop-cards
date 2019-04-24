daa98ef6eec0967d8aa6db44e86e299cbb34a676
seeo <aragonlotr5@gmail.com>
2019-04-24 18:23:31 +0800

    done with the class Card

--- cards.rb
+++ cards.rb
@@ -1,4 +1,18 @@
 class Card 
-
-
+#need to initialize this 
+    def initialize
+        @elements = []
+        (1..52).each do |i|
+        puts "adding card with value #{i} to the deck"
+        elements.push(i)
+        end
+    
+        elements.each {|i| puts "Card was #{i}"}
+        
+    end
+    def elements
+        @elements
+    end
 end 
+cards = Card.new
+p cards.elements 
