c1366ee1a177398c36111de58984cb4c4c8b55e7
helloimhd <herda58@gmail.com>
2019-04-28 19:59:01 +0800

    add cards class

--- cards.rb
+++ cards.rb
@@ -1,31 +1,64 @@
-class Card
-  attr_accessor :cards
+class Cards
   def initialize
-    @cards = Array (1..52)
+    @cards = (1..52).to_a.shuffle
+  end
 
-  end  # end of initialize
-end  # end of class
+  def get_card
+    # shuffle and get the first number
+    @cards.first
+  end
 
-deck = Card.new
+  def delete_card
+    # delete the number from the deck so cannot use anymore
+    @cards.shift
+  end
+
+end
+
+# deck = Cards.new
+# puts "befoooorrreee  "
 # p deck
 
+# deck.get_card
+# deck.delete_card
+
+# puts "afterrr "
+# p deck
+
+# deck.delete_card
+# p deck
 
 class Game
   def initialize
-    @deck = Card.new
-  end  # end of initialize
-
-  def deck
-    @deck
+    @deck = Cards.new
+    @score = 0
+    @hand_array = []
   end
 
-  def shuffle_cards
-    # shuffle the cards and select the first two cards?
-    deck.shuffle
+  def play
+    p @deck
+    @player_card = @deck.get_card
+    @hand_array << @player_card
+    @deck.delete_card
+
+    @comp_card = @deck.get_card
+    @hand_array << @comp_card
+    @deck.delete_card
+
+    p @hand_array
+
+    if @player_card > @comp_card
+      @score += 1
+    else
+      @score -= 1
+    end
   end
 
-end  # end of class
-
+  def total_score
+   p @score
+  end
+end
 
 game = Game.new
-p game.shuffle_cards
+game.play
+game.total_score
