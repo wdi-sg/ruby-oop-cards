791011e1b6164892c289917ca00450fa9f32a8a5
emijay <hilmi.johari@yahoo.com.sg>
2019-08-07 23:29:23 +0800

    able to draw two cards

--- classes.rb
+++ classes.rb
@@ -2,28 +2,35 @@ class Card
 
   def initialize
     @deck = (1 .. 52).to_a
-
   end
 
-  def showCardArray
-    puts @deck
-
-  end
-
-  def pickTwoCards
-    @twoCards = @deck.sample(2)
-    puts @twoCards
+  def getOneCard
+    @deck.shuffle.first
   end
 
 end
 
-puts "After shuffling, deck now contains: "
+class Game
 
-newCard = Card.new()
+  def initialize
+    @deck = Card.new
+    score = 0
+    cardsPlayed = []
+  end
 
-# newCard.showCardArray()
+  def playGame
+    user_hand = @deck.getOneCard
+    house_hand = @deck.getOneCard
+    puts user_hand
+    puts house_hand
+  end
 
-newCard.pickTwoCards()
+end
+
+newGame = Game.new()
+
+newGame.playGame()
+# newGame.pickTwoCards()
 # There are 52 cards in a standard deck.
 # Each card belongs to one of four suits: Spades, Hearts, Diamonds, and Clubs.
 # Each card is one of 13 ranks: Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, and King.
