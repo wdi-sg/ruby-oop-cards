2e350110ff3b1380e89fd800abc04f727a8b3946
shirleytwl <shirtanwl@gmail.com>
2019-08-07 22:20:28 +0800

    prompt user if they want to make a wager if they have enough points

--- index.rb
+++ index.rb
@@ -14,17 +14,21 @@ class Deck
 end
 
 class Game
+  	# Initialize game
 	def initialize(name)
 		@score = 0
 		@player_name = name
 		@deck = Deck.new
 	  	@playing = true
+	  	@wager = 0
 	end
 
-	def get_score
+	# Get overall score
+	def score
 		@score
 	end
 
+	# Play a round
 	def play_turn
 		player_card = @deck.get_card
 		computer_card = @deck.get_card
@@ -33,23 +37,44 @@ class Game
 
 		if player_card < computer_card
 			p "Your card is lower!"
-			@score-=1
+			if @wager == 0
+			  @score-=1
+			else
+			  @score-=@wager
+			end
 		else
 			p "Your card is higher!"
-			@score+=1
+			if @wager == 0
+				@score+=1
+			else
+			  	@score+=@wager
+			end
 		end
-		p "Current score: #{get_score} points."
+		p "Hey #{@player_name}, you have #{@score} points."
 	end
 
+	# Set wager
+	def set_wager(wager)
+	  	if @wager > @score and @wager > 1
+		  p "You do not have enough points. Please enter another amount."
+		  set_wager(gets.chomp)
+		else
+			@wager = wager.to_i
+		end
+	end
+
+	# Check if game end if score is less than 2, or deck run out, or player refused to continue
 	def check_turn
 		@score > -2 and @deck.get_deck_count > 0 and @playing
 	end
 
+	# End game message
   	def end_game
 	  p "Gamer ended!~~~~~~~~~~~"
 	  p "Your final score is #{@score.to_s} points"
 	end
 
+	# Check if continue
   	def continue_game(response)
 	  	if response == "y"
 		  	true
@@ -63,9 +88,11 @@ class Game
 	end
 end
 
+# Prompt user for name
 p "What is your name?"
 current_game = Game.new(gets.chomp)
 
+# Start the game loop
 while (true)
 	current_game.play_turn
 	if (current_game.check_turn)
@@ -73,9 +100,17 @@ while (true)
 		if current_game.continue_game(gets.chomp) == false
 		  	break
 		end
+		if (current_game.score <=1)
+		  current_game.set_wager(0)
+		else
+		  p "You may make a wager with some of your points. Default will be 1."
+		  p "Current points: #{current_game.score}. How much is your bet?"
+		  current_game.set_wager(gets.chomp)
+	  	end
 	else
 	  break
 	end
 end
 
+# If game loop stop, show end game
 current_game.end_game
