46e3329fe7707d13b2ee5027d6383c430efa8be0
Yi Xin Ang <angyixin@gmail.com>
2019-08-07 22:34:06 +0800

    don't know how to apply oop :(

--- game.rb
+++ game.rb
@@ -1,95 +1,30 @@
-#game start
-cards = *(1..52)
-computer_score = 0
-player_score = 0
-computer_hand = []
-player_hand = []
-round = 0
-p cards
+require './cards'
+require './setup'
 
-# Ask player would you like to play a game
-# Initialise game , let user know the rules
-# Ask player would you like to continue
-# game ends if use say no or either computer or user get -2
+playing = true
+
+game = game.new
 
 
-#each turn
-round = round + 1
-p "Round " + round.to_s
-#shuffle cards
-cards = cards.shuffle()
-p cards
-
-#draw a card each for computer and player
-computer_draws = cards.pop()
-player_draws = cards.pop()
-
-#show cards
-p computer_draws
-p player_draws
-
-computer_hand.push(computer_draws)
-player_hand.push(player_draws)
-
-p "computer's hand: " + computer_hand.join(",")
-p "player's hand': " + player_hand.join(",")
-
-#outcome
-if computer_draws > player_draws
-  puts "computer wins this round"
-  computer_score = computer_score + 1
-  player_score = player_score - 1
-
-else
-  puts "player wins this round"
-  player_score = player_score + 1
-  computer_score = computer_score - 1
+while player_score > -2 && computer_score > -2
+  playing = true
 end
 
-puts "computer score: " + computer_score.to_s
-puts "player score: " + player_score.to_s
 
-#for checking current cards stack
-p (cards.length).to_s + " cards left"
-
-# and the round begins again
-
-
-#each turn
-round = round + 1
-p "Round " + round.to_s
-#shuffle cards
-cards = cards.shuffle()
-p cards
-
-#draw a card each for computer and player
-computer_draws = cards.pop()
-player_draws = cards.pop()
-
-#show cards
-p computer_draws
-p player_draws
-
-computer_hand.push(computer_draws)
-player_hand.push(player_draws)
-
-p "computer's hand: " + computer_hand.join(",")
-p "player's hand': " + player_hand.join(",")
-
-#outcome
-if computer_draws > player_draws
-  puts "computer wins this round"
-  computer_score = computer_score + 1
-  player_score = player_score - 1
-
-else
-  puts "player wins this round"
-  player_score = player_score + 1
-  computer_score = computer_score - 1
+while player_score < -2 || computer_score < -2
+  playing = false
 end
 
-puts "computer score: " + computer_score.to_s
-puts "player score: " + player_score.to_s
 
-#for checking current cards stack
-p (cards.length).to_s + " cards left"
+
+
+print "What is your name? "
+name = gets.chomp
+
+puts "Hi #{name.capitalize}, would you like to play a game? yes or N"
+game_start = gets.chomp
+
+if game_start == "Y"
+  p "yay"
+else p "neh"
+end
