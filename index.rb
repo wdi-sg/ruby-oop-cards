eda0a31e0e7b44fd64dca52f7c4194f48f45ce8f
shirleytwl <shirtanwl@gmail.com>
2019-08-07 21:41:17 +0800

    prompt user if they want to continue

--- index.rb
+++ index.rb
@@ -1,59 +1,81 @@
 class Deck
-  def initialize
-    @cards = [*1..52]
-  end
+	def initialize
+		@cards = [*1..52]
+	end
 
-  def get_deck_count
-    @cards.length
-  end
+	def get_deck_count
+		@cards.length
+	end
 
-  def get_card
-    @cards = @cards.shuffle
-    @cards.pop
-  end
+	def get_card
+		@cards = @cards.shuffle
+		@cards.pop
+	end
 end
 
 class Game
-  def initialize(name)
-    @score = 0
-    @player_name = name
-    @deck = Deck.new
-  end
+	def initialize(name)
+		@score = 0
+		@player_name = name
+		@deck = Deck.new
+	  	@playing = true
+	end
 
-  def get_player_name
-    @player_name
-  end
+	def get_score
+		@score
+	end
 
-  def get_score
-    @score
-  end
+	def play_turn
+		player_card = @deck.get_card
+		computer_card = @deck.get_card
+		p @player_name+ " drew card "+player_card.to_s
+		p "Computer drew card "+computer_card.to_s
 
-  def play_turn
-    player_card = @deck.get_card
-    computer_card = @deck.get_card
-    p @player_name+ " drew card "+player_card.to_s
-    p "Computer drew card "+computer_card.to_s
+		if player_card < computer_card
+			p "Your card is lower!"
+			@score-=1
+		else
+			p "Your card is higher!"
+			@score+=1
+		end
+		p "Current score: #{get_score} points."
+	end
 
-    if player_card < computer_card
-      p "Your card is lower!"
-      @score-=1
-    else
-      p "Your card is higher!"
-      @score+=1
-    end
-  end
+	def check_turn
+		@score > -2 and @deck.get_deck_count > 0 and @playing
+	end
 
-  def check_turn
-    @score > -2 and @deck.get_deck_count > 0
-    # p @deck.get_deck_count
-  end
+  	def end_game
+	  p "Gamer ended!~~~~~~~~~~~"
+	  p "Your final score is #{@score.to_s} points"
+	end
+
+  	def continue_game(response)
+	  	if response == "y"
+		  	true
+	  	elsif response == "n"
+			false
+	  	elsif response !="y"
+			p "Please enter a valid response"
+			p "Continue? (y/n)"
+		  	continue_game(gets.chomp)
+	  	end
+	end
 end
 
 p "What is your name?"
 current_game = Game.new(gets.chomp)
 
-while (current_game.check_turn)
-  current_game.play_turn
+while (true)
+	current_game.play_turn
+	if (current_game.check_turn)
+		p "Continue? (y/n)"
+		if current_game.continue_game(gets.chomp) == false
+		  	break
+		end
+	else
+	  break
+	end
 end
 
-p "Your final score is #{current_game.get_score.to_s} points"
+current_game.end_game
