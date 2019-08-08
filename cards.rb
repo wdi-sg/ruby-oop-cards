3f00af5c9a0be4b5a4d7132dd5fec923bf986b7b
Janet <cashhpvn@yahoo.com>
2019-08-08 09:43:33 +0800

    basic(with help from solution)

---
+++ cards.rb
@@ -0,0 +1,28 @@
+class Cards
+
+	def initialize(card)
+		cards = Array(1..52)
+		@cards = cards
+
+	end
+
+	def deal
+
+		@cards.shuffle.first
+
+	end
+
+	def get_card
+		@cards.shuffle.pop
+	end
+
+end
+
+
+
+class Games
+	def initialize(players, cards)
+
+	end
+
+end
