59721e073b1b1ed37b4feae35330c4e8e605f0ad
BenBear93 <kjieun1993@gmail.com>
2019-08-08 16:34:40 +0800

    working version 1

--- solution/cards.rb
+++ solution/cards.rb
@@ -1,16 +1,74 @@
 class Cards
   def initialize
+
     @cards = []
-    52.times do |number|
-      @cards << number
+    13.times do |number|
+        @cards << [number+1, 'Spades']
+        @cards << [number+1, 'Hearts']
+        @cards << [number+1, 'Diamonds']
+        @cards << [number+1, 'Clubs']
     end
+
+    @suits = {
+        'Spades' => 4,
+        'Hearts' => 3,
+        'Diamonds' => 2,
+        'Clubs' => 1
+    }
+
+    shuffle_card
+  end
+
+  def respawn_cards
+      @cards = []
+      13.times do |number|
+          @cards << [number, 'Spades']
+          @cards << [number, 'Hearts']
+          @cards << [number, 'Diamonds']
+          @cards << [number, 'Clubs']
+      end
+
+      shuffle_card
+  end
+
+  def shuffle_card
+      @cards = @cards.shuffle
   end
 
   def show_card
-    @cards.shuffle.first
+      showcard = @cards.last
+      @cards.pop
+      showcard = "#{name_cards(showcard[0])}, #{showcard[1]}"
+      return showcard
   end
 
-  def get_card
-    @cards.shuffle.pop
+  def suits
+      @suits
   end
+
+  def name_cards(param)
+      case param
+      when 1
+          "Ace"
+      when 11
+          "Jack"
+      when 12
+          "Queen"
+      when 13
+          "King"
+      else
+          param
+      end
+  end
+
+  def cards_left
+      @cards.length
+  end
+
 end
+
+
+# @cards << [number, 'Spades']
+# @cards << [number, 'Hearts']
+# @cards << [number, 'Diamonds']
+# @cards << [number, 'Clubs']
--- solution/game.rb
+++ solution/game.rb
@@ -3,23 +3,56 @@ class Game
     @deck = Cards.new
     @score = 0
     @hands = []
+
   end
 
   def play
     user_hand = @deck.show_card
     house_hand = @deck.show_card
 
-    @hands << [user_hand, house_hand]
+    puts "******************************"
+    puts "Your Hand: #{user_hand}"
+    puts "******************************"
+    puts "House Hand: #{house_hand}"
+    puts "******************************"
 
-    if user_hand > house_hand
+    puts @deck.suits[user_hand[1] ]
+
+    if user_hand[0] > house_hand[0]
       @score += 1
+      puts "You win this hand!"
+      @hands << 'Player'
+    elsif  user_hand[0] < house_hand[0]
+      @score -= 1
+      puts "House wins!"
+      @hands << 'House'
+    elsif @deck.suits[user_hand[1] ] > @deck.suits[house_hand[1] ]
+      @score += 1
+      @hands << 'Player'
     else
       @score -= 1
+      @hands << 'House'
+    end
+    puts "\n \n \n"
+    puts "cards left #{@deck.cards_left}"
+    if @deck.cards_left < 20
+        @deck.respawn_cards
     end
   end
 
   def check_game
-    @score
+      @score
+  end
+
+  def get_hands
+      @hands
+  end
+
+  def report_summary
+      won = 0;
+      won = @hands.each.count('Player')
+      puts "\033[0m\Games Played : #{@hands.length}"
+      puts "Games Won : #{won}"
   end
 
 end
