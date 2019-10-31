886c83f931725e0e3d213c4b67d633ed8b6309c6
wongyijin8183 <wongyijin8183@gmail.com>
2019-11-01 07:57:36 +0800

    add gets.chomp feature. game doesn't end when score<-2

--- effort.rb
+++ effort.rb
@@ -3,10 +3,6 @@ class Deck
         @deck = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52].shuffle
     end
 
-    # def shuffle_deck
-    #     @deck.shuffle
-    # end
-
     def draw_card
         @deck.first
         @deck.shift
@@ -20,30 +16,23 @@ class Game
         @score = 0
     end
 
-    # def start
-    #     puts "Game has started"
-    #     puts "Score is #{@score.to_s}"
-    # end
-
     def play
         your_hand = @cards.draw_card
         my_hand = @cards.draw_card
-        @hands = [your_hand, my_hand]
+        @hands << [your_hand, my_hand]
         puts "#{your_hand} VS #{my_hand}"
         if my_hand > your_hand
             @score = @score - 1
-            puts "You Lose"
+            puts "You Lose this round"
         else
             @score = @score + 1
-            puts "You Win"
+            puts "You Win this round"
         end
         
     end
 
     def score_check
         @score
-        puts "Score is #{@score.to_s}"
-        
     end
 end
 
@@ -52,8 +41,15 @@ playing = true
 game = Game.new
 
 while playing
-    # game.start
     game.play
+    puts "do you want to try again?"
+
+    answer = gets.chomp
+    if answer == "no"
+      puts "folding"
+      next
+    end
+    puts "Score is #{game.score_check.to_s}"
     game.score_check
     if game.score_check < -2
         playing = false
