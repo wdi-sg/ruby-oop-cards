4b2a421b27e79a8860940048ecd338d11dd2422f
David M <davidmosk@gmail.com>
2019-10-31 20:39:35 +0800

    try again with sample files

--- 51cards.rb
+++ 51cards.rb
@@ -1,97 +1,67 @@
 class Cards
-
     def initialize
-        @cards = Array (1..52)
+      @cards = []
+      52.times do |number|
+        @cards << number
+      end
     end
-
-
+  
     def show_card
-        @cards.shuffle.first
+      @cards.shuffle.first
     end
-
-end
-
-
-class Game
-
-def initialize
-    @deck = Cards.new
-    @score = 0
-    @computerScore = 0
-    @turn = []
-end
-
-def play
-    @player = @deck.show_card
-    @computer = @deck.show_card
-
-
-puts "Your card is: #{player.to_s}"
-
-
-puts "Computer is: #{computer}"
-
-    if player > computer
-      @score += 1
-      @computerScore -= 1
-    else
-      @score -= 1
-      @computerScore += 1
+  
+    def get_card
+      @cards.shuffle.pop
     end
-end
-
-def player
-    return @player
-end
-
-def computer
-    return @computer
-end
-
-def check_game
-    @score
-    @computerScore
-end
-
-end
-
-
-playing = true
-
-game = Game.new
-
-while playing
-
-  game.play
-
-puts "Your current score is: #{game.check_game.to_s}"
-# puts "The computer's score is: #{game.check_game.to_s}"
-
-# if game.check_game.to_s > 
-
-puts "Do you want to play this round?"
-
-  answer = gets.chomp
-#   add a way to exit the game....
-  if answer == "q"
-  playing = false
-  end   
-
-  if answer == "no"
-    puts "folding"
-    puts "Computer's card is: #{game.computer.to_s}"
-    next
-
-else
-
-
-
-end
-
-
-
-  if game.check_game < -2
-    playing = false
-    puts "Game Over!"
   end
-end
+
+  class Game
+    def initialize
+      @deck = Cards.new
+      @score = 0
+      @hands = []
+    end
+  
+    def play
+      user_hand = @deck.show_card
+      house_hand = @deck.show_card
+  
+      @hands << [user_hand, house_hand]
+  
+      if user_hand > house_hand
+        @score += 1
+      else
+        @score -= 1
+      end
+    end
+  
+    def check_game
+      @score
+    end
+  
+  end
+  
+
+
+  playing = true
+
+  game = Game.new
+  
+  while playing
+  
+    game.play
+    puts "current score is: #{game.check_game.to_s}"
+  
+    puts "do you want to play this hand?"
+  
+    answer = gets.chomp
+    if answer == "no"
+      puts "folding"
+      next
+    end
+  
+    if game.check_game < -2
+      playing = false
+    end
+  end
+  
