e940ada3ba30ffbcc559223b83f9ca6695f80361
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-01 09:20:44 +0800

    Added game class

--- card_game.rb
+++ card_game.rb
@@ -1,5 +1,7 @@
 class Cards
 
+    attr_accessor :cards
+
     # Create an array of cards with values 1 - 52 using Code Blocks
     cards = []
     cardNum = 1
@@ -9,9 +11,28 @@ class Cards
         cardNum += 1
     end
 
-    puts cards
+    def initialize (cards)
+        @cards = cards
+    end
 
 end
 
-# Call the function
-Cards
+
+# Create Game class
+class Game
+
+    def initialize (cards_for_play)
+        # Start a game with cards
+        # @cards_for_play = Cards.new
+
+    end
+
+
+end
+
+=begin
+    # Why cannot read the card length ...
+    select_card_1 = Cards.new("card1")
+    card1 = rand(cards.length)
+    p card1
+=end
